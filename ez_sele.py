56a53042e397d8a00cc8818eb9793eb348d8daf6
BryceYzh <ads1cft@sjtu.edu.cn>
2020-01-16 17:47:51 +0800

    successfully login

--- ez_sele.py
+++ ez_sele.py
@@ -16,8 +16,8 @@ class CourseSelect:
         pass
 
     filepath = "./cap/img.png"
-    
-    
+
+
     def get_captcha(self):
         APP_ID = '18275558'
         API_KEY = 'MSLnSpkm3P7HaAmdt9kRtxex'
@@ -25,12 +25,12 @@ class CourseSelect:
 
         client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
-        
+
         options = {
                 'detect_direction' : 'true',
                 'language_type' : 'ENG',
         }
-        # 调用通用文字识别接口  
+        # 调用通用文字识别接口
         with open(self.filepath, 'rb') as fp:
             content = fp.read()
             result = client.basicAccurate(content, options)
@@ -39,8 +39,8 @@ class CourseSelect:
         print(rst)
         rst = rst.replace(' ', '')
         return rst
-    
-    
+
+
     def login(self, id, pwd):
         self.url = 'http://coursesel.umji.sjtu.edu.cn/welcome.action'
         self.browser=webdriver.Chrome("/Users/zhehaoyu/Desktop/web crawler/chromedriver-2")
@@ -52,15 +52,15 @@ class CourseSelect:
         password=browser.find_element_by_id('pass')
         username.send_keys(id)
         password.send_keys(pwd)
-        
+
         # get captcha
-        captcha=browser.find_element_by_id('captcha-img')                                        
+        captcha=browser.find_element_by_id('captcha-img')
         captcha.screenshot(self.filepath)
-        
+
         cap = self.get_captcha()
         captcha_input=browser.find_element_by_id('captcha')
         captcha_input.send_keys(cap)
-        
+
         loginBtn = browser.find_element_by_id('submit-button')
         loginBtn.click()
         time.sleep(3)
@@ -74,7 +74,7 @@ class CourseSelect:
         mark.click()
         go=browser.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div/div[2]')
         go.click()
-        
+
     def select_by_xpath(self,xpath=""):
         browser=self.browser
         xk=browser.find_element_by_xpath(xpath)
@@ -84,17 +84,16 @@ class CourseSelect:
 
 
 if __name__ == '__main__':
-    username='ads1cft'
-    pwd='yzhde1216'
+    username=''
+    pwd=''
     xp=input('input xpath: ')
-    
+
     service = CourseSelect()
     service.login(username, pwd)
     time.sleep(3)
     service.get_in()
-    
+
     time.sleep(10)
-    
+
     service.select_by_xpath(xp)
     service.browser.close()
-    
